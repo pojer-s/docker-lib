c3428c9a8def9bb773fe183388086f9328920005
spojer <spojer@castelis.com>
2017-07-13 10:21:50 +0200

    Modif wordpress

--- wordpress/genere.sh
+++ wordpress/genere.sh
@@ -2,13 +2,26 @@
 
 cd `dirname $0`
 
-for i in 4.5.3-apache 4.7.2-apache 4.7.3-apache 4.7.1-apache 4.7.0-apache 4.6.1-apache ; do
+for i in 4.5.3-apache 4.6.1-apache 4.7.0-apache 4.7.1-apache 4.7.2-apache 4.7.3-apache ; do
     mkdir -p "$i-wp"
     cd "$i-wp"
     cat <<EOF > Dockerfile
 FROM wordpress:$i
+RUN echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list
 
-RUN apt-get update && apt-get install -y libmcrypt-dev libjpeg-dev libpng12-dev libmagickwand-dev && docker-php-ext-install mcrypt pdo_mysql mysqli
+RUN apt-get update && apt-get install -y \
+        libmcrypt-dev \
+        libjpeg-dev \
+        libapache2-mod-rpaf \
+        libpng12-dev \
+        ffmpeg \
+        libmagickwand-dev \
+    && rm -rf /var/lib/apt/lists/* \
+    && docker-php-ext-install mcrypt pdo_mysql mysqli
+
+RUN a2enmod rpaf
+RUN sed -i -e "s#127.0.0.1 ::1#127.0.0.1 ::1 172.18.0.1 172.19.0.1 172.20.0.1 172.21.0.1 172.22.0.1 172.23.0.1 172.24.0.1 172.25.0.1 172.26.0.1 172.27.0.1 172.28.0.1 172.29.0.1 172.30.0.1 172.31.0.1#g" /etc/apache2/mods-enabled/rpaf.conf
+
 RUN pecl install imagick && echo "extension=imagick.so" > /usr/local/etc/php/conf.d/ext-imagick.ini
 RUN curl -o /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x /usr/local/bin/wp
 EOF
