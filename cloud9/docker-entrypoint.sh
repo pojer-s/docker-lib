826cbb696d17c2984f014487cd37a81122dc6c1c
Steven Pojer <steven@jarvis-legal.com>
2020-04-20 20:17:31 +0200

    MAJ

--- cloud9/docker-entrypoint.sh
+++ cloud9/docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -z "$WORKDIR" ]; then WORKDIR="/var/project" ; fi
-if [ -z "$PORT" ]; then PORT="8181" ; fi
-if [ -z "$LISTEN" ]; then LISTEN="0.0.0.0" ; fi
-if [ -z "$AUTH" ]; then AUTH=":" ; fi
+if [ -z "$PORT" ];    then PORT="8181"            ; fi
+if [ -z "$LISTEN" ];  then LISTEN="0.0.0.0"       ; fi
+if [ -z "$AUTH" ];    then AUTH=":"               ; fi
 
 OPTS="-b -w $WORKDIR --auth $AUTH --listen $LISTEN"
 
